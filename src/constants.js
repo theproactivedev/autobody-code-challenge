165cf06e12fb958e31f1836826d39028dda1ae2a
eirin <eiringonzales@gmail.com>
2021-04-16 17:31:13 +0800

    FLOW ADD: initial commit

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const API_BASE = 'https://game.flowautobody.com.au';
+export const USERNAME = 'flow applicant';
+export const PASSWORD = 'lets code';
+export const APPLICANT_ID = '295822270297801218';
